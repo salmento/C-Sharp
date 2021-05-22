83792e98703dbdcc2e63798f7a3f75506ab22bd4
salmento <salmentochitlango@gmail.com>
2021-05-22 08:03:21 +0200

    String and array

--- stringarray/Program.cs
+++ stringarray/Program.cs
@@ -2,6 +2,14 @@
 
 namespace stringarray
 {
+  //enums is used to declare a set of constants (value type)
+  enum friends
+  {
+    Adil,
+    Edson,
+    Camoes,
+    Yula
+  }
   class Program
   {
     static void Main(string[] args)
@@ -20,20 +28,42 @@ namespace stringarray
         Console.WriteLine(cars[i]);
       }
 
-      for (short i = 0; i <firstArray.Length; i++)
+      for (short i = 0; i < firstArray.Length; i++)
       {
-          firstArray[i] = i;
-          Console.WriteLine(firstArray[i]);
+        firstArray[i] = i;
+        Console.WriteLine(firstArray[i]);
       }
 
-        // Strings are array of characters 
-        //We can access them through indexes but we can't change them;
+      // Strings are array of characters 
+      //We can access them through indexes but we can't change them;
 
       string anyString = "You're welcome";
 
       Console.WriteLine("The last letter of you're welcome is: " + anyString[anyString.Length - 1]);
 
-    
+      //enums is used to declare a set of constants (value type)
+      friend(friends.Adil);
+      static void friend(friends name)
+      {
+
+        if (friends.Adil == name )
+        {
+          Console.WriteLine("\n Sergio Adil Dulobo");
+        } else if (friends.Camoes== name)
+        {
+          Console.WriteLine("\n Manuel Camoes");
+        } else if (friends.Edson == name){
+          Console.WriteLine("\n Edson Iranchadi");
+        } else if (friends.Yula == name ) {
+          Console.WriteLine("\n Yula Guivala");
+        } else 
+        {
+          Console.WriteLine("\n Not recognised");
+        }
+
+      }
+
+
     }
   }
 }
