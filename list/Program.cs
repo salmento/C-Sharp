2a094d00b0b608e9239a65a6dec4fe6d144d8196
salmento <salmentochitlango@gmail.com>
2021-05-23 14:30:33 +0200

    Working with list

---
+++ list/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<string> cities = new List<string>();
+      cities.AddRange(new string[] { "Maputo", "Xai Xai", "Matola", "Maxixe", "Beira" });
+      cities.ForEach( city => Console.WriteLine(city));
+    }
+  }
+}
